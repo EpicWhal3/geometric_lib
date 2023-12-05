aaa8f6086caef55087edd5d1b15cada8ebc2b7d7
EpicWhal3 <max255244@gmail.com>
2023-12-05 20:11:13 +0300

    fix: fix triangle test fail output

--- test_triangle.py
+++ test_triangle.py
@@ -5,9 +5,9 @@ import triangle
 class TriangleTestCase(unittest.TestCase):
     def test_zero_mul(self):
         res = triangle.area(10, 0)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, ValueError)
         res = triangle.area(0, 10)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, ValueError)
 
     def test_triangle_area(self):
         res = triangle.area(6, 21)
@@ -15,17 +15,17 @@ class TriangleTestCase(unittest.TestCase):
 
     def test_zero_perimeter(self):
         res = triangle.perimeter(0, 0, 0)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, ValueError)
 
     def test_any_zero(self):
         res_a = triangle.perimeter(0, 6, 7)
-        self.assertEqual(res_a, 0)
+        self.assertEqual(res_a, ValueError)
 
         res_b = triangle.perimeter(6, 0, 7)
-        self.assertEqual(res_b, 0)
+        self.assertEqual(res_b, ValueError)
 
         res_c = triangle.perimeter(6, 7, 0)
-        self.assertEqual(res_c, 0)
+        self.assertEqual(res_c, ValueError)
 
     def test_perimiteter_summ(self):
         res = triangle.perimeter(5, 8, 13)
@@ -33,4 +33,4 @@ class TriangleTestCase(unittest.TestCase):
 
     def test_negative(self):
         res = triangle.perimeter(-1, -2, -3)
-        self.assertEqual(res, 0)
+        self.assertEqual(res, ValueError)
