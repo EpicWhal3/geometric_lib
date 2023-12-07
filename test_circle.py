726b326cebf1bb7526702d0f4ffe9f049ffd88e1
EpicWhal3 <max255244@gmail.com>
2023-12-07 11:22:21 +0300

    test: fix assertEqual

--- test_circle.py
+++ test_circle.py
@@ -9,7 +9,7 @@ class CircleTestCase(unittest.TestCase):
 
     def test_area_calc(self):
         res = circle.area(5)
-        self.assertEqual(int(res), 78)
+        self.assertAlmostEqual(res, 78.5, 1)
 
     def test_zero_length(self):
         with self.assertRaises(ValueError):
@@ -17,4 +17,4 @@ class CircleTestCase(unittest.TestCase):
 
     def test_length_calc(self):
         res = circle.perimeter(8)
-        self.assertEqual(int(res), 50)
+        self.assertAlmostEqual(res, 50.2, 1)
